6e7ca06d56be880192ccb1e372d2995fa7afda34
盧彥辰 <cyril@hyper-light.net>
2021-05-03 15:08:35 +0800

    feat: 建立蘋果登入腳本
    issue #3

---
+++ Assets/Script/Model/AppleSignIn/AppleSignIn.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AppleAuth;
+using AppleAuth.Enums;
+using AppleAuth.Extensions;
+using AppleAuth.Interfaces;
+using AppleAuth.Native;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Utilities;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AppleSignIn : MonoBehaviour
+{
+
+    private const string AppleUserIdKey = "AppleUserId";
+
+    private IAppleAuthManager _appleAuthManager;
+
+    public AppleSignInHandler _AppleSignInHandler;
+
+    public Text DebugLogText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 如果是iOS裝置
+        if (AppleAuthManager.IsCurrentPlatformSupported)
+        {
+            Debug.Log("是iOS裝置");
+            DebugLogText.text = "是iOS裝置";
+            // Creates a default JSON deserializer, to transform JSON Native responses to C# instances
+            var deserializer = new PayloadDeserializer();
+            // 創建一個Apple身份驗證管理器
+            _appleAuthManager = new AppleAuthManager(deserializer);
+        }
+        else
+        {
+            Debug.Log("不是iOS裝置");
+            DebugLogText.text = "不是iOS裝置";
+            GetComponent<Button>().enabled = false;
+            GetComponent<Button>().interactable = false;
+        }
+
+        InitializeAppleSignIn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 更新AppleAuthManager
+        if (_appleAuthManager != null)
+        {
+            _appleAuthManager.Update();
+        }
+    }
+
+    /// <summary>
+    /// 點擊Apple登入按鈕
+    /// </summary>
+    public void SignInWithAppleButtonPressed()
+    {
+        Debug.Log("SignInWithAppleButtonPressed");
+        DebugLogText.text = "點擊Apple登入按鈕";
+        // 如果是iOS裝置
+        if (AppleAuthManager.IsCurrentPlatformSupported)
+        {
+            SignInWithApple();
+            Debug.Log("是iOS裝置");
+        }
+        else
+        {
+            Debug.Log("不是iOS裝置");
+        }
+    }
+
+    /// <summary>
+    /// 初始化Apple登錄
+    /// </summary>
+    private void InitializeAppleSignIn()
+    {
+
+        // 檢查當前裝置是否支持使用Apple登錄
+        if (_appleAuthManager == null)
+        {
+            Debug.Log("當前裝置沒有支持使用Apple登錄");
+            return;
+        }
+
+        // 如果突然收到憑證撤銷通知
+        _appleAuthManager.SetCredentialsRevokedCallback(result =>
+        {
+            Debug.Log("Received revoked callback " + result);
+            // 將存儲的用戶ID刪除
+            PlayerPrefs.DeleteKey(AppleUserIdKey);
+        });
+
+        if (PlayerPrefs.HasKey(AppleUserIdKey)) // 如果有可用的Apple用戶ID，獲取憑證狀態
+        {
+            var storedAppleUserId = PlayerPrefs.GetString(AppleUserIdKey);
+            CheckCredentialStatusForUserId(storedAppleUserId);
+        }
+
+    }
+
+    /// <summary>
+    /// 檢查憑證狀態以獲取UserId
+    /// </summary>
+    private void CheckCredentialStatusForUserId(string appleUserId)
+    {
+        // 如果有可用的Apple ID，則檢查憑證狀態
+        _appleAuthManager.GetCredentialState(
+            appleUserId,
+            state =>
+            {
+                switch (state)
+                {
+                    // 如果獲得授權，使用該用戶ID登錄
+                    case CredentialState.Authorized:
+                        //SetupAppleSignIn(appleUserId, null);
+                        return;
+
+                    // 如果被撤消或找不到，需要重新登錄Apple
+                    // 將存儲的用戶ID刪除
+                    case CredentialState.Revoked:
+                    case CredentialState.NotFound:
+                        //SetupLoginMenuForSignInWithApple();
+                        PlayerPrefs.DeleteKey(AppleUserIdKey);
+                        return;
+                }
+            },
+            error =>
+            {
+                // 獲取憑證狀態時出錯
+                var authorizationErrorCode = error.GetAuthorizationErrorCode();
+                DebugLogText.text = "Error authorizationErrorCode: " + authorizationErrorCode.ToString() + " " + error.ToString();
+                Debug.Log("Error authorizationErrorCode: " + authorizationErrorCode.ToString() + " " + error.ToString());
+            });
+    }
+
+    /// <summary>
+    /// 登入Apple
+    /// </summary>
+    private void SignInWithApple()
+    {
+        var loginArgs = new AppleAuthLoginArgs(LoginOptions.IncludeEmail | LoginOptions.IncludeFullName);
+
+        _appleAuthManager.LoginWithAppleId(
+            loginArgs,
+            credential =>
+            {
+                // 如果使用Apple登錄成功，則可獲得帶有用戶ID，Name和Email的憑證
+                PlayerPrefs.SetString(AppleUserIdKey, credential.User);
+                SetupAppleSignIn(credential.User, credential);
+            },
+            error =>
+            {
+                // Sign in with Apple 失敗
+                var authorizationErrorCode = error.GetAuthorizationErrorCode();
+                Debug.Log("Sign in with Apple failed " + authorizationErrorCode.ToString() + " " + error.ToString());
+            });
+    }
+
+    /// <summary>
+    /// 設置AppleSignIn驗證資料
+    /// </summary>
+    /// <param name="appleUserId"></param>
+    /// <param name="credential"></param>
+    private void SetupAppleSignIn(string appleUserId, ICredential credential)
+    {
+        _AppleSignInHandler.SetupAppleData(appleUserId, credential, this);
+    }
+
+    /// <summary>
+    /// AppleSignIn Apple端回傳成功
+    /// </summary>
+    /// <param name="code"></param>
+    /// <param name="token"></param>
+    public void CallBackForAppleSignIn(string returnLog)
+    {
+        DebugLogText.text = "AppleSignIn Apple端回傳成功: " + returnLog;
+        Debug.Log("AppleSignIn Apple端回傳成功: " + returnLog);
+    }
+
+    /// <summary>
+    /// AppleSignIn Apple端回傳失敗
+    /// </summary>
+    /// <param name="error"></param>
+    public void CallBackForAppleSignInError(string error)
+    {
+        DebugLogText.text = "AppleSignIn Apple端回傳失敗: " + error;
+        Debug.Log("AppleSignIn Apple端回傳失敗: " + error);
+    }
+}
---
+++ Assets/Script/Model/AppleSignIn/AppleSignInHandler.cs
@@ -0,0 +1,120 @@
+﻿using AppleAuth.Enums;
+using AppleAuth.Extensions;
+using AppleAuth.Interfaces;
+using System;
+using System.Text;
+using UnityEngine;
+
+
+[Serializable]
+public class AppleSignInHandler
+{
+    private string code = ""; //AppleSignIn 的 AuthorizationCode
+    private string token = ""; //AppleSignIn 的 IdentityToken
+
+    /// <summary>
+    /// AppleSignIn 取得使用者資料
+    /// </summary>
+    /// <param name="appleUserID"></param>
+    /// <param name="receivedCredential"></param>
+    /// <param name="appleSignIn"></param>
+    public void SetupAppleData(string appleUserID, ICredential receivedCredential, AppleSignIn appleSignIn)
+    {
+        var stringBuilder = new StringBuilder();
+
+        if (receivedCredential == null)
+        {
+            // 使用者憑證沒有授權
+            stringBuilder.AppendLine("使用者: " + appleUserID + " 憑證沒有授權");
+            appleSignIn.CallBackForAppleSignIn(stringBuilder.ToString());
+            return;
+        }
+
+        var appleIdCredential = receivedCredential as IAppleIDCredential;
+        var passwordCredential = receivedCredential as IPasswordCredential;
+
+        if (appleIdCredential != null)
+        {
+            //回傳AppleSignIn資訊
+            stringBuilder.AppendLine("---------------------回傳 AppleSignIn 資訊-----------------------");
+            stringBuilder.AppendLine("appleIdCredential.User: " + appleIdCredential.User);
+            stringBuilder.AppendLine("Real user status: " + appleIdCredential.RealUserStatus.ToString());
+
+            if (appleIdCredential.State != null)
+            {
+                stringBuilder.AppendLine("State:  " + appleIdCredential.State);
+            }
+
+            //IdentityToken
+            if (appleIdCredential.IdentityToken != null)
+            {
+                var identityToken = Encoding.UTF8.GetString(appleIdCredential.IdentityToken, 0, appleIdCredential.IdentityToken.Length);
+                token = identityToken;
+                stringBuilder.AppendLine("Identity Token (" + appleIdCredential.IdentityToken.Length + " bytes)");
+                stringBuilder.AppendLine("Identity Token:  " + identityToken);
+            }
+
+            //AuthorizationCode
+            if (appleIdCredential.AuthorizationCode != null)
+            {
+                var authorizationCode = Encoding.UTF8.GetString(appleIdCredential.AuthorizationCode, 0, appleIdCredential.AuthorizationCode.Length);
+                code = authorizationCode;
+                stringBuilder.AppendLine("Authorization Code (" + appleIdCredential.AuthorizationCode.Length + " bytes)");
+                stringBuilder.AppendLine("Authorization Code:  " + authorizationCode);
+            }
+
+            if (appleIdCredential.AuthorizedScopes != null)
+            {
+                stringBuilder.AppendLine("Authorized Scopes::  " + string.Join(", ", appleIdCredential.AuthorizedScopes));
+            }
+
+            //Email: 首次登入時可選擇是否隱藏，若隱藏則為 null
+            if (appleIdCredential.Email != null)
+            {
+                //You can test this again by revoking credentials in Settings
+                stringBuilder.AppendLine();
+                stringBuilder.AppendLine("Email: 首次登入時可選擇是否隱藏，若隱藏則為 null");
+                stringBuilder.AppendLine("Email:  " + appleIdCredential.Email);
+            }
+
+            if (appleIdCredential.FullName != null)
+            {
+                //NAME RECEIVED: YOU WILL ONLY SEE THIS ONCE PER SIGN UP. SEND THIS INFORMATION TO YOUR BACKEND!
+                //You can test this again by revoking credentials in Settings
+                var fullName = appleIdCredential.FullName;
+                stringBuilder.AppendLine();
+                stringBuilder.AppendLine("Name:  " + fullName.ToLocalizedString());
+                stringBuilder.AppendLine("Name (Short):  " + fullName.ToLocalizedString(PersonNameFormatterStyle.Short));
+                stringBuilder.AppendLine("Name (Medium):  " + fullName.ToLocalizedString(PersonNameFormatterStyle.Medium));
+                stringBuilder.AppendLine("Name (Long):  " + fullName.ToLocalizedString(PersonNameFormatterStyle.Long));
+                stringBuilder.AppendLine("Name (Abbreviated):  " + fullName.ToLocalizedString(PersonNameFormatterStyle.Abbreviated));
+
+                if (appleIdCredential.FullName.PhoneticRepresentation != null)
+                {
+                    var phoneticName = appleIdCredential.FullName.PhoneticRepresentation;
+                    stringBuilder.AppendLine("Name:  " + phoneticName.ToLocalizedString());
+                    stringBuilder.AppendLine("Name (Short):  " + phoneticName.ToLocalizedString(PersonNameFormatterStyle.Short));
+                    stringBuilder.AppendLine("Name (Medium):  " + phoneticName.ToLocalizedString(PersonNameFormatterStyle.Medium));
+                    stringBuilder.AppendLine("Name (Long):  " + phoneticName.ToLocalizedString(PersonNameFormatterStyle.Long));
+                    stringBuilder.AppendLine("Name (Abbreviated):  " + phoneticName.ToLocalizedString(PersonNameFormatterStyle.Abbreviated));
+                }
+            }
+            stringBuilder.AppendLine("----------------------------------------------------------------");
+            appleSignIn.CallBackForAppleSignIn(stringBuilder.ToString());
+        }
+        else if (passwordCredential != null)
+        {
+            stringBuilder.AppendLine("USERNAME/PASSWORD RECEIVED (iCloud?)");
+            stringBuilder.AppendLine("Username: " + passwordCredential.User);
+            stringBuilder.AppendLine("Password: " + passwordCredential.Password);
+            appleSignIn.CallBackForAppleSignInError(stringBuilder.ToString());
+        }
+        else
+        {
+            stringBuilder.AppendLine("未知的用戶憑證 " + receivedCredential.User);
+            appleSignIn.CallBackForAppleSignInError(stringBuilder.ToString());
+        }
+    }
+
+
+}
