98d2c4f3a6575cc6e5df38598ce223f01376c2b8
augus <augus-0331@hyper-light.net>
2021-05-03 14:56:21 +0800

    feat: 新增測試bean
    issue #4

---
+++ Assets/Script/Bean/JsonUtilityTestBean.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace Assets.Script.Bean
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class JsonUtilityTestBean
+    {
+        //
+        [JsonProperty("Name")]
+        public string Name;
+    }
+}
