a1965382932d065f3eb4f1c326a77f987a6b5a05
盧彥辰 <cyril@hyper-light.net>
2021-05-04 15:26:23 +0800

    feat: 加入Editor資料夾及GoogleSignInPostBuild
    1. 解決 Xcode 的 exception 'NSInvalidArgumentException', reason: 'You must specify |clientID| for |GIDSignIn|' 錯誤
    
    issue #6

---
+++ Assets/Editor/GoogleSignInPostBuild.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+#if UNITY_IOS
+using UnityEditor.iOS.Xcode;
+#endif
+using UnityEngine;
+
+public static class GoogleSignInPostBuild
+{
+
+    [PostProcessBuild(999)]
+    public static void OnPostProcessBuild(BuildTarget buildTarget, string buildPath)
+    {
+#if UNITY_IOS
+        var projPath = buildPath + "/Unity-iPhone.xcodeproj/project.pbxproj";
+        var proj = new PBXProject();
+        proj.ReadFromFile(projPath);
+        var targetGuid = proj.GetUnityMainTargetGuid();
+        proj.AddFileToBuild(targetGuid, proj.AddFile("Data/Raw/GoogleService-Info.plist", "GoogleService-Info.plist"));
+        proj.WriteToFile(projPath);
+#endif
+
+    }
+
+}
