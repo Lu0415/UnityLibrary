3ff7179d1472836f8ad7e8d9966217c847eb9de1
盧彥辰 <cyril@hyper-light.net>
2021-05-04 09:31:38 +0800

    feat: 腳本綁定、按鈕狀態判斷及註解

--- Assets/Script/Model/FBSignIn/FBSignIn.cs
+++ Assets/Script/Model/FBSignIn/FBSignIn.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using Facebook.Unity;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,11 +10,40 @@ public class FBSignIn : MonoBehaviour
     private string getAccessToken = "";
     private string getEmail = "";
 
+    //序列化狀態
+    Text FBMessageText;
+
+    //反序列化狀態
+    Button FBLoginButton;
+
+    //序列化內容
+    Button FBLogoutButton;
+
     /// <summary>
     /// 初始化 Facebook SDK
     /// </summary>
     void Awake()
     {
+
+        if (GameObject.Find("/Canvas/Content/Image/Message").TryGetComponent<Text>(out Text textSerialization))
+        {
+            FBMessageText = textSerialization;
+        }
+
+        if (GameObject.Find("/Canvas/Content/LogIn").TryGetComponent<Button>(out Button textFBLoginButton))
+        {
+            FBLoginButton = textFBLoginButton;
+        }
+
+        if (GameObject.Find("/Canvas/Content/LogOut").TryGetComponent<Button>(out Button textFBLogoutButton))
+        {
+            FBLogoutButton = textFBLogoutButton;
+        }
+
+
+        FBLoginButton.interactable = false;
+        FBLogoutButton.interactable = false;
+
         if (FB.IsInitialized)
         {
             // Already initialized, signal an app activation App Event
@@ -31,6 +61,11 @@ public class FBSignIn : MonoBehaviour
     /// </summary>
     protected void HandleResult(IResult result)
     {
+        var stringBuilder = new StringBuilder();
+
+        getAccessToken = "";
+        getEmail = "";
+
         if (result == null)
         {
             isFacebookSignIn = false;
@@ -40,11 +75,13 @@ public class FBSignIn : MonoBehaviour
         // Some platforms return the empty string instead of null.
         if (!string.IsNullOrEmpty(result.Error))
         {
+            //回傳錯誤
             isFacebookSignIn = false;
             Debug.Log("Error Response:\n" + result.Error);
         }
         else if (result.Cancelled)
         {
+            //取消
             isFacebookSignIn = false;
             Debug.Log("Cancelled Response:\n" + result.RawResult);
         }
@@ -53,37 +90,26 @@ public class FBSignIn : MonoBehaviour
             //FB 登入成功
             isFacebookSignIn = true;
             Debug.Log("Success Response:\n" + result.RawResult);
-
             getAccessToken = (string)result.ResultDictionary["access_token"];
-
+            stringBuilder.AppendLine("Success getAccessToken:\n" + getAccessToken);
             //如果Email沒有授權獲認證則填空
-            try
+            if (result.ResultDictionary.ContainsKey("email"))
             {
                 getEmail = (string)result.ResultDictionary["email"];
             }
-            catch
-            {
-                getEmail = "";
-            }
+            
+            stringBuilder.AppendLine("Success getEmail:\n" + getEmail);
 
-            //送出登入資訊
-            //AotHelper.EnsureType<FacebookLogin>();
-            //FacebookLogin data = new FacebookLogin();
-            //data.AccessToken = getAccessToken;
-            //data.Email = getEmail;
-            //data.AppVersion = Application.version;
-            //data.UserAgent = GameControl._instance.GetUserAgent();
-            //Debug.Log("UserAgent: " + data.UserAgent);
-            //Debug.Log("FacebookLogin: " + JsonConvert.SerializeObject(data));
-            //GameControl _GameControl = GameControl._instance.GetComponent<GameControl>();
-            //_GameControl.SendEncryptedMessage(BaseConstants.EVENT_FACEBOOK_LOGIN, data);
-
+            FBLoginButton.interactable = false;
+            FBLogoutButton.interactable = true;
         }
         else
         {
             Debug.Log("Empty Response\n");
         }
+        FBMessageText.text = stringBuilder.ToString();
         Debug.Log("FB 登入回傳狀態 result: " + result.ToString());
+        
         Debug.Log(isFacebookSignIn);
     }
 
@@ -102,16 +128,24 @@ public class FBSignIn : MonoBehaviour
         if (AccessToken.CurrentAccessToken != null)
         {
             Debug.Log("FB 初始化完畢 CurrentAccessToken: " + AccessToken.CurrentAccessToken);
+            FBMessageText.text = "FB 初始化完畢 CurrentAccessToken: " + AccessToken.CurrentAccessToken;
             isFacebookSignIn = true;
+
+            FBLoginButton.interactable = false;
+            FBLogoutButton.interactable = true;
         }
         else
         {
             isFacebookSignIn = false;
+
+            FBLoginButton.interactable = true;
+            FBLogoutButton.interactable = false;
         }
     }
 
     private void OnHideUnity(bool isGameShown)
     {
+        FBMessageText.text = string.Format("Success Response: OnHideUnity Called {0}\n", isGameShown);
         Debug.Log(string.Format("Success Response: OnHideUnity Called {0}\n", isGameShown));
     }
 
@@ -120,7 +154,7 @@ public class FBSignIn : MonoBehaviour
     /// </summary>
     public void SignInWithFacebookButtonPressed()
     {
-        CallFBLogout();
+        FBMessageText.text = "FB 登入按鈕點擊事件";
 
         if (FB.IsInitialized) //已經初始化了
         {
@@ -134,6 +168,7 @@ public class FBSignIn : MonoBehaviour
     /// </summary>
     private void CallFBLogin()
     {
+        FBMessageText.text = "FB 登入方法執行";
         FB.LogInWithReadPermissions(new List<string>() { "public_profile", "email", "user_friends" }, HandleResult);
     }
     /*
@@ -152,6 +187,7 @@ public class FBSignIn : MonoBehaviour
     /// </summary>
     public void CallFBLogout()
     {
+        FBMessageText.text = "FB 登出方法執行";
         FB.LogOut();
         isFacebookSignIn = false;
     }
