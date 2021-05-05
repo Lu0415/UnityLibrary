395a3e1ee5c49d5e9b2c305eab071c702825156c
盧彥辰 <cyril@hyper-light.net>
2021-05-05 17:26:39 +0800

    feat:  WebSocket實作
    1. scoket controller
    2. 介面場景控制
    3. 使用簡單的EventHandler
    
    issue #8

---
+++ Assets/Script/Model/WebSocket/WebSocketController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+public class WebSocketController : MonoBehaviour
+{
+    
+    public event EventHandler<string> WebSocketReturnMsg;
+
+    private string _socketEchoURL = "wss://echo.websocket.org";
+
+    private WebSocket _ws;
+
+    private bool _wsConnectState;
+
+    //靜態單例
+    public static WebSocketController _instance;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);//儲存功能節點
+        }
+        else if (this != _instance)
+        {
+            Destroy(gameObject);
+        }
+
+        _wsConnectState = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 建立Socket連線
+    /// </summary>
+    public void CreateSocket()
+    {
+        
+        _ws = new WebSocket(_socketEchoURL);
+        _ws.WaitTime = new TimeSpan(0, 3, 0);
+        _ws.SslConfiguration.EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12;
+        _ws.OnOpen += SocketOpen;
+        _ws.OnMessage += SocketOnMessage;
+        //_ws.OnError += SocketOnError;
+        //_ws.OnClose += SocketOnClose;
+        _ws.ConnectAsync();
+        Debug.Log("開始連接");
+        
+    }
+
+    /// <summary>
+    /// 成功連線
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void SocketOpen(object sender, EventArgs e)
+    {
+        _wsConnectState = true;
+        Debug.Log("成功連線 SocketOpen");
+        if (WebSocketReturnMsg != null) WebSocketReturnMsg(this, "成功連線 SocketOpen");
+        _ws.EmitOnPing = true;
+    }
+
+    /// <summary>
+    /// 接收Websockett訊息
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e">訊息</param>
+    void SocketOnMessage(object sender, MessageEventArgs e)
+    {
+        if (e.IsPing)
+        {
+            Debug.Log("接收Websockett訊息: Ping");
+            if (WebSocketReturnMsg != null) WebSocketReturnMsg(this, "Echo: Ping" + "\n");
+        }
+        else
+        {
+            Debug.Log("接收Websockett訊息: " + e.Data);
+            if (WebSocketReturnMsg != null) WebSocketReturnMsg(this, "Echo: " + e.Data + "\n");
+        }
+    }
+
+    /// <summary>
+    /// Socket送出訊息
+    /// </summary>
+    /// <param name="msg"></param>
+    public void SocketSendMsg(string msg)
+    {
+        _ws.Send(msg);
+    }
+}
---
+++ Assets/Script/Model/WebSocket/WebSocketScenesController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using WebSocketSharp;
+
+public class WebSocketScenesController : MonoBehaviour
+{
+    
+    private StringBuilder _stringBuilder;
+
+    //顯示狀態及文字
+    Text SocketMessageText;
+
+    //文字輸入框
+    InputField MsgInputField;
+
+    //送出文字按鈕
+    Button SendMsgButton;
+
+    ScrollRect ScrollControl;
+
+    void Start()
+    {
+
+        if (GameObject.Find("/Canvas/Content/Image/Message").TryGetComponent<Text>(out Text _socketMessageText))
+        {
+            SocketMessageText = _socketMessageText;
+        }
+
+        if (GameObject.Find("/Canvas/Content/InputField").TryGetComponent<InputField>(out InputField _msgInputField))
+        {
+            MsgInputField = _msgInputField;
+            MsgInputField.onValueChanged.AddListener(delegate { InputFieldOnValueChanged(); });
+        }
+
+        if (GameObject.Find("/Canvas/Content/SendMsg").TryGetComponent<Button>(out Button _sendMsgButton))
+        {
+            SendMsgButton = _sendMsgButton;
+        }
+
+        if (GameObject.Find("/Canvas/Content/Image").TryGetComponent<ScrollRect>(out ScrollRect _scrollControl))
+        {
+            ScrollControl = _scrollControl;
+        }
+
+        WebSocketController._instance.CreateSocket();
+
+        _stringBuilder = new StringBuilder();
+        _stringBuilder.Append("開始連接socket" + "\n");
+
+        SocketMessageText.text = _stringBuilder.ToString();
+        
+        WebSocketController._instance.WebSocketReturnMsg += WebSocketReturnMsgListener;
+
+        SendMsgButton.interactable = false;
+        
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+
+    /// <summary>
+    /// 文字輸入框輸入文字時
+    /// </summary>
+    public void InputFieldOnValueChanged()
+    {
+        if (MsgInputField.text.Length == 0)
+        {
+            SendMsgButton.interactable = false;
+        }
+        else
+        {
+            SendMsgButton.interactable = true;
+        }
+        Debug.Log("InputFieldOnValueChanged: " + MsgInputField.text + "\n");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    /// <summary>
+    /// 點擊送出訊息按鈕
+    /// </summary>
+    public void SendMessageButtonPressed()
+    {
+        WebSocketController._instance.SocketSendMsg(MsgInputField.text);
+        _stringBuilder.Append("Send: " + MsgInputField.text + "\n");
+        SocketMessageText.text = _stringBuilder.ToString();
+
+        MsgInputField.text = "";
+
+        StartCoroutine(nameof(ScrollToBottom));
+    }
+    
+    /// <summary>
+    /// 接收Websockett訊息
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="msg"></param>
+    void WebSocketReturnMsgListener(object sender, string msg)
+    {
+        Debug.Log("接收Websockett訊息: " + msg);
+        _stringBuilder.Append(msg);
+
+        SocketMessageText.text = _stringBuilder.ToString();
+
+        StartCoroutine(nameof(ScrollToBottom));
+    }
+
+
+    IEnumerator ScrollToBottom()
+    {
+        yield return new WaitForEndOfFrame();
+        ScrollControl.verticalNormalizedPosition = 0f;
+    }
+}
